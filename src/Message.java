a3c1b33b04b737341c020b92b896bccc53d3b329
davidigandan <davidigandan@gmail.com>
2024-06-07 22:40:20 +0100

    Message method

--- src/Message.java
+++ src/Message.java
@@ -1,3 +1,35 @@
+import java.time.LocalDateTime;
+
 public class Message {
-    
+    private String encryptedMessage;
+    private String signature;
+    private LocalDateTime timestamp;
+
+    public Message(String encryptedMessage, String signature, LocalDateTime timestamp) {
+        this.encryptedMessage = encryptedMessage;
+        this.signature = signature;
+        this.timestamp = timestamp;
+    }
+
+    public String getEncryptedMessage() {
+        return encryptedMessage;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "encryptedMessage='" + encryptedMessage + '\'' +
+                ", signature='" + signature + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
 }
