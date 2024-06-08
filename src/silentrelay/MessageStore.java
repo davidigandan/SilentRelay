9b60ce6528a27df38b796d3eee6160ae093c9699
davidigandan <davidigandan@gmail.com>
2024-06-08 17:33:35 +0100

    Store and retrieve logic corrected

--- src/silentrelay/MessageStore.java
+++ src/silentrelay/MessageStore.java
@@ -8,8 +8,12 @@ import java.time.LocalDateTime;
 public class MessageStore {
     private HashMap<String, ArrayList<Message>> userMessages = new HashMap<>();
 
-    public void addMessage(String hashedRecieverUserID, String encryptedMessage) {
+    public void storeEncryptedMessage(String hashedRecieverUserID, String encryptedMessage) {
         Message messageEntry = new Message(encryptedMessage, LocalDateTime.now());
+        userMessages.computeIfAbsent(hashedRecieverUserID, k -> new ArrayList<>()).add(messageEntry);
+    }
 
+    public ArrayList<Message> getEncryptedMessages(String userId) {
+        return userMessages.getOrDefault(userId, new ArrayList<>());
     }
 }
