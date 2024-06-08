c61c102bb38f1a394d62e3f5e64bf94b1ba19f12
davidigandan <davidigandan@gmail.com>
2024-06-08 20:24:07 +0100

    Added Bool return

--- src/silentrelay/MessageStore.java
+++ src/silentrelay/MessageStore.java
@@ -8,12 +8,17 @@ import java.time.LocalDateTime;
 public class MessageStore {
     private HashMap<String, ArrayList<Message>> userMessages = new HashMap<>();
 
-    public void storeEncryptedMessage(String hashedRecieverUserID, String encryptedMessage) {
-        Message messageEntry = new Message(encryptedMessage, LocalDateTime.now());
-        userMessages.computeIfAbsent(hashedRecieverUserID, k -> new ArrayList<>()).add(messageEntry);
+    public Boolean storeEncryptedMessage(String hashedRecieverUserID, String encryptedMessage) {
+        try {
+            Message messageEntry = new Message(encryptedMessage, LocalDateTime.now());
+            userMessages.computeIfAbsent(hashedRecieverUserID, k -> new ArrayList<Message>()).add(messageEntry);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public ArrayList<Message> getEncryptedMessages(String userId) {
-        return userMessages.getOrDefault(userId, new ArrayList<>());
+        return userMessages.getOrDefault(userId, new ArrayList<Message>());
     }
 }
