7cfab7945ab80f1e1515cf823d538e7ad7f3b9ac
davidigandan <davidigandan@gmail.com>
2024-06-10 15:47:44 +0100

    Started signing messages to be sent to the client

--- src/silentrelay/Message.java
+++ src/silentrelay/Message.java
@@ -1,4 +1,10 @@
 package silentrelay;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.time.LocalDateTime;
 
 // Contains Encrypted Message and Timestamp
@@ -19,8 +25,14 @@ public class Message {
         return timestamp;
     }
 
-    public String generateSignature() {
-        return "lol";
+    public String generateSignature() throws IOException {
+        try {
+            byte[] keyBytes = Files.readAllBytes(Paths.get("server.prv"));
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = keyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        }
+        
     }
 
     @Override
